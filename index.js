b253a30007cd3837fb7d737d767d8356a665b2fb
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-08-06 14:56:53 +0530

    testing for loading html template

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const electron = require("electron");
-const { app } = electron;
+const { app, BrowserWindow } = electron;
 
 app.on("ready", ()=>{
 	console.log('hello');
+	// creating a sample window with default configuration
+	const mainWindow = new BrowserWindow({});
+	mainWindow.loadURL(`file://${__dirname}/index.html`);
 })
