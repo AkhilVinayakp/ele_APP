16a13bdd96b57a42e94e55443370c20b7b88a711
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-08-08 22:05:48 +0530

    finding the duration of the video file using ffmpeg

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const electron = require("electron");
-const { app, BrowserWindow } = electron;
-
+const { app, BrowserWindow, ipcMain } = electron;
+const ffmpeg = require("fluent-ffmpeg")
 app.on("ready", ()=>{
 	console.log('hello');
 	// creating a sample window with default configuration
-	const mainWindow = new BrowserWindow({});
+	const mainWindow = new BrowserWindow({
+		webPreferences:{
+			nodeIntegration:true
+		}
+	});
 	mainWindow.loadURL(`file://${__dirname}/index.html`);
+})
+// adding event listener for using ipcMain and using the channel send from the
+ipcMain.on("getVideoLength",(event,path)=>{
+	console.log("event received")
+	ffmpeg.ffprobe(path, (err, metadata)=>{
+		console.log(metadata.format.duration);
+		console.log("error"+err);
+	});
 })
